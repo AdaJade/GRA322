aea718d57b841efd0b2ac5bb4108237549aa905a
AdaJade <awalke40@asu.edu>
2022-04-28 00:10:27 -0700

    program website updates

---
+++ program-book/js/scripts.js
@@ -0,0 +1,12 @@
+
+const cursorPointed = document.querySelector('.pointed');
+
+const moveCursor = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+}
+
+window.addEventListener('mousemove', moveCursor)
